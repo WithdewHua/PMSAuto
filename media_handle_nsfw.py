0aef3a72d0d8850c256b67bc929d342134987270
WithdewHua <i@10101.io>
2024-06-18 23:14:59 +0800

    fix: minor bugs

--- media_handle_nsfw.py
+++ media_handle_nsfw.py
@@ -6,7 +6,6 @@ from time import sleep
 
 from log import logger
 from media_handle import rename_media, send_scan_request
-from scheduler import scheduler
 
 
 src_path = "/Media/Inbox/NSFW/Done"
@@ -32,21 +31,7 @@ for src_dir in src_dirs:
     logger.info(f"Removed folder: {os.path.join(src_path, src_dir)}")
     scan_folders.append(dst_dir)
 
-for idx, scan_folder in enumerate(set(scan_folders)):
-    run_date = datetime.datetime.now() + datetime.timedelta(minutes=3, seconds=30 * idx)
-    scheduler.add_job(
-        send_scan_request,
-        args=(scan_folder,),
-        trigger="date",
-        run_date=run_date,
-        misfire_grace_time=60,
-        jobstore="default",
-        id=f"scan_task_at_{run_date}",
-    )
-    logger.debug(f"Added scheduler job: next run at {str(run_date)}")
-
-while True:
-    if not scheduler.get_jobs():
-        break
+for scan_folder in set(scan_folders):
+    send_scan_request(scan_folder)
     sleep(30)
 
