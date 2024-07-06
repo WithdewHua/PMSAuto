71190ed758012f32999f4c750cdfc8988c20cf6a
WithdewHua <i@10101.io>
2024-07-06 14:25:34 +0800

    fix: adjust nsfw folder strcture

--- media_handle_nsfw.py
+++ media_handle_nsfw.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import re
 
 from time import sleep
 
@@ -15,20 +16,37 @@ dst_dirs = os.listdir(dst_path)
 
 scan_folders = []
 
-# number/actors/number
+release_cre = re.compile(r"<release>([\d-]+)</release>")
+# actors/number
 for src_dir in src_dirs:
-    series_num = src_dir.split("-")[0].upper()
-    actors = os.listdir(os.path.join(src_path, src_dir))[0]
-    if actors in ["failed", "佚名"]:
+    if src_dir in ["failed", "佚名"]:
         continue
-    dst_dir = os.path.join(dst_path, series_num, actors, src_dir)
-    if os.path.exists(dst_dir):
-        logger.warning(f"Folder already exists: {dst_dir}")
-        continue
-    rename_media(os.path.join(src_path, src_dir, actors, src_dir), dst_dir)
-    shutil.rmtree(os.path.join(src_path, src_dir))
-    logger.info(f"Removed folder: {os.path.join(src_path, src_dir)}")
-    scan_folders.append(dst_dir)
+    numbers = os.listdir(os.path.join(src_path, src_dir))
+    for number in numbers:
+        c_nfo = os.path.join(src_path, src_dir, number, f"{number}-C.nfo")
+        no_c_nfo = os.path.join(src_path, src_dir, number, f"{number}.nfo")
+        if os.path.exists(c_nfo):
+            nfo = c_nfo
+        elif os.path.exists(no_c_nfo):
+            nfo = no_c_nfo
+        else:
+            nfo = None
+        if nfo is None:
+            logger.warning(f"{number}'s NFO not found, skip...")
+            continue
+        with open(nfo, "r") as f:
+            date_match = release_cre.search(f.read())
+        if not date_match:
+            logger.warning(f"Failed to match {number}'s release data, skip...")
+            continue
+        year, month, _ = date_match.group(1).split("-")
+
+        dst_dir = os.path.join(dst_path, year, month, number)
+        if os.path.exists(dst_dir):
+            logger.warning(f"Folder already exists: {dst_dir}")
+            continue
+        rename_media(os.path.join(src_path, src_dir, number), dst_dir)
+        scan_folders.append(dst_dir)
 
 for scan_folder in set(scan_folders):
     send_scan_request(scan_folder)
